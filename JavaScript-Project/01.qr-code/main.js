38b28c7190b467ea695cb3c6ec95bc2bb5939f80
Putri Mila <putrimiila.fx@gmail.com>
2024-08-29 19:26:37 +0800

    add QR code JS

--- JavaScript-Project/01.qr-code/main.js
+++ JavaScript-Project/01.qr-code/main.js
@@ -1,1 +1,19 @@
+document.getElementById('generate-btn').addEventListener('click', function() {
+    let url = document.getElementById('url-links').value;
+    let qrCodeContainer = document.getElementById('qr-code');
 
+    qrCodeContainer.innerHTML = '';
+
+    if (url.trim() === '') {
+        alert('Please enter a valid URL.');
+        return;
+    }
+
+    new QRCode(qrCodeContainer, {
+        text: url,
+        width: 250,
+        height: 250,
+        colorDark: '#FFFFFF',
+        colorLight: "rgba(0, 0, 0, 0)"
+    });
+});
